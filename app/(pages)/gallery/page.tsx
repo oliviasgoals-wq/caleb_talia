43518ee3e4bf1dd191a9b69cee4b2952de901ae7
oliviasgoals-wq <oliviasgoals@gmail.com>
2026-03-21 21:30:07 +0200

    Video added

--- app/(pages)/gallery/page.tsx
+++ app/(pages)/gallery/page.tsx
@@ -15,6 +15,18 @@ export default function Gallery(){
             <Navbar theme="light" />
             <h1 className="text-center text-6xl font-tangerine"> <span className="text-[#008800]">Our</span> Gallery</h1>
 
+            <div className="my-8 max-w-8xl mx-auto flex justify-center">
+                <video
+                    className="w-auto h-150 object-cover rounded-xl shadow-xl"
+                    controls
+                    autoPlay
+                    loop
+                >
+                    <source src="/photos/video.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+
             <div className="pt-10 columns-2 lg:columns-4 gap-4 [column-fill:_balance] max-w-8xl mx-auto">
                 {weddingImages.map((image, index) => (
                     <div key={image.id} className="break-inside-avoid mb-4 animate-float" style={{ animationDelay: `${index * 0.1}s`, animationDuration: '3s', }}> 
